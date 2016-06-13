ee17020a192dd512932b0188bb906e49b5aee7e1
IrgendeinPc <IrgendeinPc@DESKTOP-NC7AJ3N>
2016-06-13 13:58:45 +0200

    Image was adet, Leftpart

---
+++ 4Gewinnt/4Gewinnt/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _4Gewinnt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void DrawImage(PaintEventArgs p)
+        {
+            Image Left = Image.FromFile("4Gewinnt\\Resource\\4Wins-LeftPart");
+
+            Graphics myGraphics = p.Graphics;
+            myGraphics.DrawImage(Left, 10, 10);
+        }
+    }
+}
